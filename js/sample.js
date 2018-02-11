1d8e69e5f9ea75ab9c5c96090f19f13a9d149138
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-02-11 16:04:04 +0300

    v0.87.1
    UX rework to reduce clicks - auto push to OOSList if radio checked.

--- js/sample.js
+++ js/sample.js
@@ -18,13 +18,19 @@ var sku = function(name, oos, notEnough) {
 
 quant();
 
+
+
 for (var i = 0; i < skuList.length; i++) {
     for (var radio = 0; radio < skuList[i].querySelectorAll('input[type="radio"]').length; radio++) {
         skuList[i].querySelectorAll('input[type="radio"]')[radio].addEventListener('change', function() {
-           skuName = this.parentNode.parentNode.querySelector('p').innerText;
+            skuName = this.parentNode.parentNode.querySelector('p').innerText;
+            var trig = this,
+                par = this.parentNode.parentNode,
+                trigs = par.querySelectorAll('input[type="radio"]');
+        
            for (var key = 0; key <= oosList.length; key++) {
-               if (oosList[0] === undefined || oosList[key].name !== skuName) {
-                   if (this.parentNode.parentNode.querySelectorAll('input[type="radio"]')[0].checked === true) {
+               if (oosList[0] === undefined) {
+                   if (trigs[0].checked === true) {
                        skuOOS = true;
                        /*if (this.parentNode.parentNode.querySelector('input[type="number"]') !== null) {
                             this.parentNode.parentNode.querySelector('input[type="number"]').remove();
@@ -32,20 +38,83 @@ for (var i = 0; i < skuList.length; i++) {
                    } else {
                        skuOOS = false;
                    }
-                   if (this.parentNode.parentNode.querySelectorAll('input[type="radio"]')[1].checked === true) {
+                   if (trigs[1].checked === true) {
                        skuNotEnough = true;                    
                        //skuQuantity = this.parentNode.querySelector('input[type="number"]').value.toString();
                    } else {
                        skuNotEnough = false;
                        //skuQuantity = "";
                    }
-                   
+        
+                   par.style.backgroundColor = "#87ab89"
+        
                    addSku();
-               } //else if (key !== 0 || oosList[key].name !== skuName) {
-                  // break;                   
-               //}
-           } 
+                   console.log(par.getElementsByTagName('button'));
+                   par.getElementsByTagName('button')[0].style.display = "block"
+                   par.getElementsByTagName('button')[1].style.display = "block"
+                   return;
+               } else if (oosList[0] !== undefined) {
+                   for (var k in oosList) {
+                       if (oosList[k].name === skuName) {
+                           if (trigs[0].checked === true) {
+                           oosList[key].oos = true;
+                           oosList[key].notEnough = false;
+                           oosList[key].quantity = "oos";
+                           return;
+                       } else if (trigs[1].checked === true) {
+                          oosList[key].notEnough = true;
+                          oosList[key].oos = false;                          
+                          return;
+                      }
+        
+                       } else {
+                           if (trigs[0].checked === true) {
+                               skuOOS = true;
+                               /*if (this.parentNode.parentNode.querySelector('input[type="number"]') !== null) {
+                                    this.parentNode.parentNode.querySelector('input[type="number"]').remove();
+                                    }*/  //remove on change event 
+                           } else {
+                               skuOOS = false;
+                           }
+                           if (trigs[1].checked === true) {
+                               skuNotEnough = true;                    
+                               //skuQuantity = this.parentNode.querySelector('input[type="number"]').value.toString();
+                           } else {
+                               skuNotEnough = false;
+                               //skuQuantity = "";
+                           }
+        
+                           par.style.backgroundColor = "#87ab89"
+        
+                           for (var ky in oosList) {
+                               if (oosList[ky].name === skuName) {
+                                   console.log(skuList)
+                                   return;
+                               } 
+                           }
+                           addSku();
+                           par.getElementsByTagName('button')[0].style.display = "block";
+                           par.getElementsByTagName('button')[1].style.display = "block";
+                           return;
+                       }
+                   }
+        
+               } 
+           }
         });
+//        if (this.parentNode.parentNode.querySelector(input[type="number"]) !== null) {
+//            this.parentNode.parentNode.querySelector(input[type="number"]).addEventListener('change', function () {
+//                var self = this,
+//                    par = this.parentNode,
+//                    name = par.querySelector('p').innerText;
+//                for (var key in skuList) {
+//                    if (skuList[key].name === name) {
+//                        skuList[key].quantity = self.innerText;
+//                    }
+//                }
+//                console.log(skuList);
+//            });
+//        }
     }
 }
 
